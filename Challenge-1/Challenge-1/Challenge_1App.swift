2329b3f9d27974ab703953a24a4eab6fe235fba4
Mabast Osman <mabastu@yahoo.com>
2021-05-07 16:31:06 +0300

    Add files via upload

---
+++ Challenge-1/Challenge-1/Challenge_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_1App.swift
+//  Challenge-1
+//
+//  Created by Mabast on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
